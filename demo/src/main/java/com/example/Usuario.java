9cc62168fb30071731fcdf7eb70d38eabc302bd7
LuizaGmz <luizalimagomes04@gmail.com>
2025-11-21 04:42:00 -0300

    CRUD

--- demo/src/main/java/com/example/Usuario.java
+++ demo/src/main/java/com/example/Usuario.java
@@ -1,6 +1,10 @@
 package com.example;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
@@ -20,5 +24,23 @@ public class Usuario {
         this.idade = idade;
     }
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
